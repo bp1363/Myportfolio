71c52bf99844a1b276da403b8d17beb30a4b5c1e
Brajpal Singh <brajpal1363@email.com>
2026-06-01 09:55:48 +0530

    Updates

---
+++ precache-manifest.1b15649eedfb8331b1fddf9064562348.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "430d57ace064e657bb2c5b74ae93c010",
+    "url": "/Myportfolio/index.html"
+  },
+  {
+    "revision": "a583e964e11c5d02807d",
+    "url": "/Myportfolio/static/css/main.11d03a0b.chunk.css"
+  },
+  {
+    "revision": "8711447209db0c752cee",
+    "url": "/Myportfolio/static/js/2.fbf3b411.chunk.js"
+  },
+  {
+    "revision": "12adf5ee6e135646e379",
+    "url": "/Myportfolio/static/js/3.8ba39429.chunk.js"
+  },
+  {
+    "revision": "a583e964e11c5d02807d",
+    "url": "/Myportfolio/static/js/main.f5cffe7e.chunk.js"
+  },
+  {
+    "revision": "acf62225197f9fed9f92",
+    "url": "/Myportfolio/static/js/runtime~main.dc384c5f.js"
+  },
+  {
+    "revision": "5a1e5c8ae40c902349a0da6baf00c784",
+    "url": "/Myportfolio/static/media/Brajpal.5a1e5c8a.pdf"
+  },
+  {
+    "revision": "30150231d5c08cdb8c9404dd97c4af67",
+    "url": "/Myportfolio/static/media/bp.30150231.png"
+  }
+]);
